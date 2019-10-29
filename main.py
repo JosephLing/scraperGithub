c86e9ea59601965a5c2a88e349b2f58c89cfc249
Joe Ling - uni laptop <josephling11@gmail.com>
2019-10-29 17:10:27 +0000

    unique csv for scrapping the data off multiple pages
    - fixed encoding bug
    - made it so that multiple pages and timeouts have been added in
    - readme and config content is in file
    - need to have , converted into a different character as repo
    descriptions can have , them in whcih breaks things.... as well as \n
    chars breaking the csv format

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from github import Github, GithubException
 from dotenv import load_dotenv
 from os import getenv
 import csv
+import time
 load_dotenv()
 
-def saveRepos(repos, name="repo"):
+def saveRepos(repos, contents, name="repo"):
+    data = []
     keys = ["name", "id", "description", "language", "open_issues", "stargazers_count", "topics", "watchers_count"]
     methods = ["get_readme"]
+    index = 0
     for repo in repos:
         print("saving >>> {}".format(repo.name))
 
@@ -16,22 +19,61 @@ def saveRepos(repos, name="repo"):
         except GithubException as e:
             pass
         
-        dictionary = dict([(k, getattr(repo, k)) for k in keys])
+        dictionary = dict([(k, fixEncoding(getattr(repo, k))) for k in keys])
         dictionary["readme"] = readme
+        dictionary["config"] = contents[index]
+        
+        data.append(dictionary)
 
-        field = list(dictionary.keys())
-        
+        index += 1
+    return data
+
+def writeToCsv(data, name):
+    if len(data) > 1:
+        field = list(data[0].keys())
+
         with open("{}.csv".format(name), "a") as csvfile:
+
             writer = csv.DictWriter(csvfile,fieldnames=field)
-            writer.writerow(dictionary)
-        
+            writer.writeheader()
+            for d in data:
+                writer.writerow(d)
+    else:
+        print("no data found")
+
+def fixEncoding(value):
+    if isinstance(value, str):
+        return value.encode("utf8")
+    else:
+        return value
+
 def getReposFromFiles(files):
     repos = []
     contents = []
     for file in files:
         repos.append(file.repository)
+        print("reading >>> {}".format(repos[len(repos)-1].name))
         contents.append(file.content)
-    return repos,contents
+    return repos, contents
+
+def getConfigStuff(g, search, name):
+    name += time.strftime("%X")
+    temp = g.search_code(search)
+    index = 0
+    page = temp.get_page(index)
+    while len(page):
+        print("getting page: {}".format(index))
+        repos, content = getReposFromFiles(page)
+        if len(repos) > 1:
+            writeToCsv(saveRepos(repos, content), name)
+        else:
+            print("no data found")
+        
+        index += 1
+        page = temp.get_page(index)
+        time.sleep(500)
+
+
 
 def main():
     GITHUB_TOKEN = getenv("GITHUB_TOKEN")
@@ -39,11 +81,7 @@ def main():
         print("place a github token in the .env file")
     else:
         g = Github(GITHUB_TOKEN)
-        temp = g.search_code("filename:travis.yml")
-        print(temp.get_page(0))
-        repos, content = getReposFromFiles(temp.get_page(0))
-        saveRepos(repos, "travis2")
-
+        getConfigStuff(g, "filename:travis.yml", "allTheTravis")
 
 if __name__ == "__main__":
     main()
