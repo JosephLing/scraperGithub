53ada658d5d4b6232d10ab6e6e0f4a103833022f
Joe Ling - uni laptop <josephling11@gmail.com>
2019-10-29 14:59:46 +0000

    got travis.yml search working and saving
    search_code is needed in order to get the filename:query search working
    as that doesn't work on search_repos... for some reason. The docs are a
    little vague but I what they mean is that repo searching can only be
    done on name and description which kind of makes sense.
    
    Note: encoding bug needs to sorted along with getting the config into
    the csv file as well. Luckily that will be base64 encoded as well so
    should be pretty simple to do

--- main.py
+++ main.py
@@ -1,20 +1,49 @@
 from github import Github, GithubException
 from dotenv import load_dotenv
 from os import getenv
+import csv
 load_dotenv()
 
-GITHUB_TOKEN = getenv("GITHUB_TOKEN")
-if GITHUB_TOKEN is None:
-    print("place a github token in the .env file")
-else:
-    g = Github(GITHUB_TOKEN)
-    repos = g.get_user().get_repos()
+def saveRepos(repos, name="repo"):
     keys = ["name", "id", "description", "language", "open_issues", "stargazers_count", "topics", "watchers_count"]
     methods = ["get_readme"]
     for repo in repos:
-        readme = 0
+        print("saving >>> {}".format(repo.name))
+
+        readme = ""
         try:
-            readme = repo.get_readme().size
+            readme = repo.get_readme().content
         except GithubException as e:
             pass
-        print("{}\t readme: {}".format("\t".join(["{} {}".format(k, getattr(repo, k)) for k in keys]), readme))
+        
+        dictionary = dict([(k, getattr(repo, k)) for k in keys])
+        dictionary["readme"] = readme
+
+        field = list(dictionary.keys())
+        
+        with open("{}.csv".format(name), "a") as csvfile:
+            writer = csv.DictWriter(csvfile,fieldnames=field)
+            writer.writerow(dictionary)
+        
+def getReposFromFiles(files):
+    repos = []
+    contents = []
+    for file in files:
+        repos.append(file.repository)
+        contents.append(file.content)
+    return repos,contents
+
+def main():
+    GITHUB_TOKEN = getenv("GITHUB_TOKEN")
+    if GITHUB_TOKEN is None:
+        print("place a github token in the .env file")
+    else:
+        g = Github(GITHUB_TOKEN)
+        temp = g.search_code("filename:travis.yml")
+        print(temp.get_page(0))
+        repos, content = getReposFromFiles(temp.get_page(0))
+        saveRepos(repos, "travis2")
+
+
+if __name__ == "__main__":
+    main()
