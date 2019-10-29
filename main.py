dcfb2be67576d79c61af2dde44640c98b09136ab
Joe Ling - uni laptop <josephling11@gmail.com>
2019-10-29 20:00:42 +0000

    implemented limit on pages and added other ci/cd to be scrapped
    now just need to run it for an hour or two for each config.... :(

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import csv
 import time
 load_dotenv()
 
+TIMEOUT = 5
+MAX_NO_OF_PAGES = 1
+
 def saveRepos(repos, contents, name="repo"):
     data = []
     keys = ["name", "id", "description", "language", "open_issues", "stargazers_count", "topics", "watchers_count"]
@@ -29,12 +32,12 @@ def saveRepos(repos, contents, name="repo"):
     return data
 
 def writeToCsv(data, name):
-    if len(data) > 1:
+    if len(data) >= 1:
         field = list(data[0].keys())
 
         with open("{}.csv".format(name), "a") as csvfile:
 
-            writer = csv.DictWriter(csvfile,fieldnames=field)
+            writer = csv.DictWriter(csvfile,fieldnames=field, quoting=csv.QUOTE_MINIMAL)
             writer.writeheader()
             for d in data:
                 writer.writerow(d)
@@ -57,11 +60,13 @@ def getReposFromFiles(files):
     return repos, contents
 
 def getConfigStuff(g, search, name):
-    name += time.strftime("%X")
+    print("----------------------------------------")
+    print("getting data for: {} will take around 2mins".format(name))
+    name += time.strftime("%X").replace(":","_")
     temp = g.search_code(search)
     index = 0
     page = temp.get_page(index)
-    while len(page):
+    while len(page) >= 1 and index < MAX_NO_OF_PAGES:
         print("getting page: {}".format(index))
         repos, content = getReposFromFiles(page)
         if len(repos) > 1:
@@ -71,7 +76,9 @@ def getConfigStuff(g, search, name):
         
         index += 1
         page = temp.get_page(index)
-        time.sleep(500)
+        print("sleeping for: {}s to avoid 403 errors due to rate limiting".format(TIMEOUT))
+        time.sleep(TIMEOUT)
+    print("finished going through {} pages of results".format(MAX_NO_OF_PAGES))
 
 
 
@@ -81,7 +88,18 @@ def main():
         print("place a github token in the .env file")
     else:
         g = Github(GITHUB_TOKEN)
-        getConfigStuff(g, "filename:travis.yml", "allTheTravis")
+        # writeToCsv([{"a":"cat,fish,dog"}], "test.csv")
+        # getConfigStuff(g, "extension:.yml filename:travis.yml", "allTheTravis")
+        # getConfigStuff(g, "extension:.yml path:.circle/ filename:config.yml", "allTheCircles")
+        # getConfigStuff(g, "extension:.yml path:.github/workflows name", "githubActions")
+        # getConfigStuff(g, "extension:.yml filename:.gitlab-ci.yml", "gitlab")
+        # getConfigStuff(g, "filename:JenkinsFile", "jenkinsPipeline")
+        # getConfigStuff(g, "filename:.cirrus.yml", "cirrus") # cirrus https://cirrus-ci.org/examples/
+        # getConfigStuff(g, "path:.cds version", "cds") # https://ovh.github.io/cds/docs/tutorials/init_workflow_with_cdsctl/ more CD than CI though
+        getConfigStuff(g, "path:.teamcity version", "teamcity") # note the content of the configuration will be xml and it will be messy most likely as team city has lots of files for all the things
+
+        # note: GoCd seems to be used so little that I can't find any good examples of it its config
+        # combined with the fact that they allow .json and .yml ah!
 
 if __name__ == "__main__":
     main()
