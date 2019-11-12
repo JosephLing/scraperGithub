615482317b3a779807d3f66ca20b9962dbf646ab
Joe Ling - uni laptop <josephling11@gmail.com>
2019-11-12 12:49:15 +0000

    created checker to give stats on the local directory

--- checker.py
+++ checker.py
@@ -1,17 +1,41 @@
 import os
 import csv
+import sys
 
-def check(content):
-    lines = content.readlines()
-    for line in lines:
-        # need to use csv lib for this
-        if len(line.split(",")):
-            pass
+from os import listdir
+from os.path import isfile, join
+
+maxInt = sys.maxsize
+
+while True:
+    # decrease the maxInt value by factor 10
+    # as long as the OverflowError occurs.
+
+    try:
+        csv.field_size_limit(maxInt)
+        break
+    except OverflowError:
+        maxInt = int(maxInt / 10)
+
+
+def check(filename):
+    with open(filename, "r", encoding="utf-8") as cat:
+        reader = csv.DictReader(cat)
+        i = 0
+        keysLength = 0
+        for row in reader:
+            if i == 0:
+                keysLength = len(row.keys())
+            i += 1
+        print("name: %s%slines: %d%skeys: %d" % (filename, " " * (40 - len(filename)), i, " " * (10-len(str(i))), keysLength))
+
 
 def main():
-    for file in os:
-        with open(file, 'r', encoding='utf8') as fileData:
-            check(fileData)
+    mypath = "."
+    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith(".csv")]
+    for f in onlyfiles:
+        check(f)
+
 
 if __name__ == "__main__":
     main()
