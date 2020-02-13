382df1643350e402c388b3572c83aa54a096192f
Joe Ling - uni laptop <josephling11@gmail.com>
2020-02-13 14:00:17 +0000

    tidied up matlab test render

--- src/matlab_test_render.py
+++ src/matlab_test_render.py
@@ -8,19 +8,13 @@ graphing guides:
 
 """
 
-import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.style as mplstyle
-import numpy as np
 import csvReader
 
 
-# mplstyle.use(['dark_background', 'ggplot', 'fast'])
-
-
-def spread_of_data_sub_to_stars(axis, data):
-    plt.xlabel('stars')
-    plt.ylabel('subscribers')
+def spread_of_data_sub_to_stars(plot, data):
+    plot.xlabel('stars')
+    plot.ylabel('subscribers')
 
     x = []
     y = []
@@ -28,95 +22,35 @@ def spread_of_data_sub_to_stars(axis, data):
         x.append(int(line.get("stargazers_count")))
         y.append(int(line.get("subscribers_count")))
     print(len(x))
-    plt.scatter(x, y, s=1, alpha=0.75)
+    plot.scatter(x, y, s=1, alpha=0.75)
 
-    plt.title("Sample of repositories from github")
+    plot.title("Sample of repositories from github")
 
-    # plt.legend()
-
-    # plt.show()
-    plt.savefig(f'spreadscatter_starsandsubs.pdf')
+    plot.legend()
+    return plot
 
 
-def spread_over_time_stars_pgf(axis, data):
-    """
-    this seems bad atm
-    :param axis:
-    :param data:
-    :return:
-    """
-    # matplotlib.use("pgf")
-    #
-    # matplotlib.rcParams.update({
-    #     "pgf.texsystem": "pdflatex",
-    #     'font.family': 'serif',
-    #     'text.usetex': True,
-    #     'pgf.rcfonts': False,
-    # })
+def save_as_pdf(plot, name):
+    plot.savefig(f'{name}.pdf', format="pdf")
 
-    plt.ylabel('stars')
-
+def spread_over_time_stars(plot, data):
+    plot.ylabel('stars')
     x = []
     for line in data:
         x.append(int(line.get("stargazers_count")))
 
-    print(len(x))
     x.sort()
+    plot.scatter(list(range(len(x))), x, s=0.5)
 
-    count = 0
-    for a in x:
-        if a == 0:
-            count += 1
+    plot.title("Sample of repositories from github")
 
-    print(count)
-    print(x[:10])
-    plt.scatter(list(range(len(x))), x, s=0.5)
-
-    plt.title("Sample of repositories from github")
-    export_to_latex(plt, "dogs")
-
-    # plt.legend()
-
-    plt.show()
-    # plt.savefig(f'latex thing.pgf')
-
-
-def spread_over_time_stars_pdf(axis, data):
-    """
-    this seems bad atm
-    :param axis:
-    :param data:
-    :return:
-    """
-    plt.ylabel('stars')
-
-    x = []
-    for line in data:
-        x.append(int(line.get("stargazers_count")))
-
-    print(len(x))
-    x.sort()
-
-    plt.scatter(list(range(len(x))), x, s=0.5)
-
-    plt.title("Sample of repositories from github")
-
-    name = "sample over time"
-
-    plt.savefig(f'{name}.pdf', format="pdf")
-
-
-
-def export_to_latex(plot, name):
-    pass
-
+    return plot
 
 
 def main():
     data = csvReader.readfile("combined0.csv")
-
-    spread_over_time_stars_pdf(plt, data)
-    # spread_of_data_sub_to_stars(plt,data)
+    save_as_pdf(spread_of_data_sub_to_stars(plt, data), "sub vs stars")
+    save_as_pdf(spread_over_time_stars(plt, data), "spread over time")
 
 
 if __name__ == '__main__':
